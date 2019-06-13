676eb62cbc6da011c368f8a46555637efd5c7bf9
Laiai_jc <1547349124@qq.com>
2019-06-13 21:24:08 +0800

    新增jsp

--- src/main/java/com/ljc/blog/controller/AircleGroupController.java
+++ src/main/java/com/ljc/blog/controller/AircleGroupController.java
@@ -1,14 +1,26 @@
 package com.ljc.blog.controller;
 
+import com.ljc.blog.entity.AircleGroup;
 import com.ljc.blog.service.AircleGroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class AircleGroupController {
     @Autowired
     private AircleGroupService aircleGroupService;
-    public  void  insertGroup(){
+    @RequestMapping("/insertGroup")
+    public  String  insertGroup(List<AircleGroup> listAir){
+        if(listAir.size()>0){
+           for (AircleGroup r:listAir){
+               aircleGroupService.insertGroup(r);
+           }
+           return "";
+        }
+        return  null;
 
     }
 }
---
+++ src/main/webapp/WEB-INF/view/Admin/insertGroup.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: l_computer
+  Date: 2019/6/13
+  Time: 21:18
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>新增分组</title>
+</head>
+<body>
+<form action="insertGroup.action" method="get">
+    标题：<input type="text" name="groupName">
+    标签
+
+</form>
+</body>
+</html>
